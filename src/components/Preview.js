91c9c7659523e30f85073aa92e7ea916b28ddbfe
GenaPlem <shainkill1337@gmail.com>
2024-02-12 16:02:08 +0000

    Add Preview component base structure

---
+++ src/components/Preview.js
@@ -0,0 +1,10 @@
+export const Preview = () => {
+    return (
+        <div className="preview">
+            <h2 className="preview__header">Preview</h2>
+            <div className="preview__content">lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor
+                sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+            </div>
+        </div>
+    )
+}
