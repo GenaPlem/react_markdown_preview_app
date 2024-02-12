369f0ee2ffadf6044da89d6c74d86ccbb97b809a
GenaPlem <shainkill1337@gmail.com>
2024-02-12 16:01:54 +0000

    Add Editor component base structure

---
+++ src/components/Editor.js
@@ -0,0 +1,8 @@
+export const Editor = () => {
+    return (
+        <div className="editor">
+            <h2 className="editor__header">Editor</h2>
+            <textarea className="editor__textarea"></textarea>
+        </div>
+    )
+}
