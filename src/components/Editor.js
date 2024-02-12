6dd7ea0513bce44399aa3b121d1f3b80b157d774
GenaPlem <shainkill1337@gmail.com>
2024-02-12 16:47:35 +0000

    Add onChange function

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,8 +1,9 @@
-export const Editor = () => {
+export const Editor = ({setMarkdownText, text}) => {
     return (
         <div className="editor">
             <h2 className="editor__header">Editor</h2>
-            <textarea className="editor__textarea"></textarea>
+            <textarea className="editor__textarea" onChange={(e) => setMarkdownText(e.target.value)}
+                      value={text}></textarea>
         </div>
     )
 }
