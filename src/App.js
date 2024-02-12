ad01b2dd9b1fee2c064894ea8645f16b52970126
GenaPlem <shainkill1337@gmail.com>
2024-02-12 16:03:22 +0000

    Add components into App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.scss';
+import {Editor} from "./components/Editor";
+import {Preview} from "./components/Preview";
 
 function App() {
     return (
         <div className="App">
-            <h1>Markdown Previewer</h1>
+            <h1 className="title">Markdown Previewer</h1>
+            <Editor/>
+            <Preview/>
         </div>
     );
 }
