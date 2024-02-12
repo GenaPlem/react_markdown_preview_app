e7ab9c5abef4d1bb81add965dfe1e89bc9eda7b4
GenaPlem <shainkill1337@gmail.com>
2024-02-12 18:59:49 +0000

    Add markdown settings

--- src/App.js
+++ src/App.js
@@ -4,9 +4,26 @@ import {Preview} from "./components/Preview";
 import {useState} from "react";
 import {marked} from "marked";
 
+import hljs from 'highlight.js';
 
 function App() {
     const [markdown, setMarkdown] = useState('')
+
+    const renderer = new marked.Renderer();
+    marked.setOptions({
+        renderer: renderer,
+        highlight: function (code, lang) {
+            const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+            return hljs.highlight(code, {language}).value;
+        },
+        pedantic: false,
+        gfm: true, // Включает GitHub Flavored Markdown
+        breaks: true, // Перенос строки на <br>, если это необходимо
+        sanitize: false, // Отключите, если хотите разрешить вставку сырого HTML
+        smartLists: true,
+        smartypants: false,
+        xhtml: false
+    });
 
     const setMarkdownText = (value) => {
         setMarkdown(value)
