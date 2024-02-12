8bfc00f64b3bb37b81efed069d750db14afca3a9
GenaPlem <shainkill1337@gmail.com>
2024-02-12 16:47:11 +0000

    Add state and function to set markdown text from textarea

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.scss';
 import {Editor} from "./components/Editor";
 import {Preview} from "./components/Preview";
+import {useState} from "react";
 
 function App() {
+    const [markdown, setMarkdown] = useState('Init')
+
+    const setMarkdownText = (value) => {
+        setMarkdown(value)
+    }
+
     return (
         <div className="App">
             <h1 className="title">Markdown Previewer</h1>
-            <Editor/>
-            <Preview/>
+            <Editor setMarkdownText={setMarkdownText} text={markdown}/>
+            <Preview text={markdown}/>
         </div>
     );
 }
